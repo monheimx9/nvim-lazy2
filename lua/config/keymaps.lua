e78a03aea6e7bfe5c3e3fd89d2da33e120035ea4
Jeremy Delley <jeremy.delley@imex-dtp.com>
2023-11-04 17:29:44 +0100

    update config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local Util = require("lazyvim.util")
+local newmap = Util.safe_keymap_set
+newmap("n", "<leader>rr", "<cmd>RustDebuggables<cr>", { desc = "[R]ust Debug Start" })
