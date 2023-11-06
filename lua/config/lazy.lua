1238b9cfab908d39bec84f23de9fb4a3e389ef17
Jeremy Delley <jeremy.delley@imex-dtp.com>
2023-11-06 20:59:30 +0100

    edgy add

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.ui.edgy" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- import/override with your plugins
     { import = "plugins" },
