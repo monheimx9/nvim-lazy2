30979dc74c9ab2127ab2a4dfff67fa389d21106b
Jeremy Delley <jeremy.delley@imex-dtp.com>
2024-05-06 10:53:33 +0200

    Rust DAP

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,4 +1,5 @@
 return {
+
   {
     "neovim/nvim-lspconfig",
     opts = {
@@ -6,12 +7,58 @@ return {
         -- Ensure mason installs the server
         rust_analyzer = {
           keys = {
-            { "<leader>rK", "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
-            { "<leader>rR", "<cmd>RustCodeAction<cr>", desc = "Code Action (Rust)" },
-            { "<leader>rr", "<cmd>RustDebuggables<cr>", desc = "Run Debuggables (Rust)" },
+            { "<leader>uK", "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
+            { "<leader>uF", "<cmd>RustCodeAction<cr>", desc = "Code Action (Rust)" },
+            { "<leader>uJ", "<cmd>RustDebuggables<cr>", desc = "Run Debuggables (Rust)" },
           },
         },
       },
     },
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^4", -- Recommended
+    ft = { "rust" },
+    opts = {
+      server = {
+        on_attach = function(_, bufnr)
+          local il_h = require("lsp-inlayhints").setup()
+          il_h.on_attach(_, bufnr)
+          vim.keymap.set("n", "<leader>cR", function()
+            vim.cmd.RustLsp("codeAction")
+          end, { desc = "Code Action", buffer = bufnr })
+          vim.keymap.set("n", "<leader>dr", function()
+            vim.cmd.RustLsp("debuggables")
+          end, { desc = "Rust debuggables", buffer = bufnr })
+        end,
+        default_settings = {
+          -- rust-analyzer language server configuration
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              runBuildScripts = true,
+            },
+            -- Add clippy lints for Rust.
+            checkOnSave = {
+              allFeatures = true,
+              command = "clippy",
+              extraArgs = { "--no-deps" },
+            },
+            procMacro = {
+              enable = true,
+              ignored = {
+                ["async-trait"] = { "async_trait" },
+                ["napi-derive"] = { "napi" },
+                ["async-recursion"] = { "async_recursion" },
+              },
+            },
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
+    end,
   },
 }
