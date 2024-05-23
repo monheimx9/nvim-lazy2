1b5195f716db2b58173bd476645e33f262fddf8b
Jeremy Delley <jeremy.delley@imex-dtp.com>
2024-05-23 20:44:20 +0200

    add treesitter-context
    keep function signature at the top of the screen

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.ui.edgy" },
+    { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- import/override with your plugins
     { import = "plugins" },
