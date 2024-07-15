e8c9639ccef680efa6bc616f35465190a10e4f14
Jeremy Delley <jeremy.delley@imex-dtp.com>
2024-07-15 20:45:25 +0200

    ricing catppuccin thanks to anonymous dude

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,99 @@
+-- return {
+--   "LazyVim/LazyVim",
+--   opts = {
+--     colorscheme = "tokyonight-night",
+--   },
+-- }
+--
+--
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = { colorscheme = "catppuccin" },
-  },
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = true,
+      color_overrides = {
+        macchiato = {
+          rosewater = "#efc9c2",
+          flamingo = "#ebb2b2",
+          pink = "#f2a7de",
+          mauve = "#b889f4",
+          red = "#ea7183",
+          maroon = "#ea838c",
+          peach = "#f39967",
+          yellow = "#eaca89",
+          green = "#96d382",
+          teal = "#78cec1",
+          sky = "#91d7e3",
+          sapphire = "#68bae0",
+          blue = "#739df2",
+          lavender = "#a0a8f6",
+          text = "#b5c1f1",
+          subtext1 = "#a6b0d8",
+          subtext0 = "#959ec2",
+          overlay2 = "#848cad",
+          overlay1 = "#717997",
+          overlay0 = "#63677f",
+          surface2 = "#505469",
+          surface1 = "#3e4255",
+          surface0 = "#2c2f40",
+          base = "#1a1c2a",
+          mantle = "#141620",
+          crust = "#0e0f16",
+        },
+        show_end_of_buffer = false,
+        integration_default = false,
+        integrations = {
+          barbecue = { dim_dirname = true, bold_basename = true, dim_context = false, alt_background = false },
+          cmp = true,
+          gitsigns = true,
+          hop = true,
+          illuminate = { enabled = true },
+          native_lsp = { enabled = true, inlay_hints = { background = true } },
+          neogit = true,
+          neotree = true,
+          semantic_tokens = true,
+          treesitter = true,
+          treesitter_context = true,
+          vimwiki = true,
+          which_key = true,
+        },
+        highlight_overrides = {
+          all = function(colors)
+            return {
+              CurSearch = { bg = colors.sky },
+              IncSearch = { bg = colors.sky },
+              CursorLineNr = { fg = colors.blue, style = { "bold" } },
+              DashboardFooter = { fg = colors.overlay0 },
+              TreesitterContextBottom = { style = {} },
+              WinSeparator = { fg = colors.overlay0, style = { "bold" } },
+              ["@markup.italic"] = { fg = colors.blue, style = { "italic" } },
+              ["@markup.strong"] = { fg = colors.blue, style = { "bold" } },
+              Headline = { style = { "bold" } },
+              Headline1 = { fg = colors.blue, style = { "bold" } },
+              Headline2 = { fg = colors.pink, style = { "bold" } },
+              Headline3 = { fg = colors.lavender, style = { "bold" } },
+              Headline4 = { fg = colors.green, style = { "bold" } },
+              Headline5 = { fg = colors.peach, style = { "bold" } },
+              Headline6 = { fg = colors.flamingo, style = { "bold" } },
+              rainbow1 = { fg = colors.blue, style = { "bold" } },
+              rainbow2 = { fg = colors.pink, style = { "bold" } },
+              rainbow3 = { fg = colors.lavender, style = { "bold" } },
+              rainbow4 = { fg = colors.green, style = { "bold" } },
+              rainbow5 = { fg = colors.peach, style = { "bold" } },
+              rainbow6 = { fg = colors.flamingo, style = { "bold" } },
+            }
+          end,
+        },
+      },
+    })
+
+    vim.api.nvim_command("colorscheme catppuccin-macchiato")
+  end,
 }
